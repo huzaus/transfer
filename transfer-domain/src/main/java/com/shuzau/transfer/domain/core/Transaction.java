821bd7bfa67554479f313b4ee46a1f04a7250dd3
huzaus <siarhei.huzau@grandparade.co.uk>
2018-05-18 16:08:16 +0200

    Replace equal, hashCode and toString methods with lombok

--- transfer-domain/src/main/java/com/shuzau/transfer/domain/core/Transaction.java
+++ transfer-domain/src/main/java/com/shuzau/transfer/domain/core/Transaction.java
@@ -1,8 +1,11 @@
 package com.shuzau.transfer.domain.core;
 
+import lombok.EqualsAndHashCode;
 import lombok.NonNull;
+import lombok.ToString;
 
-
+@EqualsAndHashCode
+@ToString
 public class Transaction {
 
     private final AccountId accountId;
@@ -10,7 +13,7 @@ public class Transaction {
     private final Money amount;
     private final Transaction previousTransaction;
 
-    private Transaction(AccountId accountId, TransactionId id, Money amount, Transaction previousTransaction) {
+    protected Transaction(AccountId accountId, TransactionId id, Money amount, Transaction previousTransaction) {
         this.accountId = accountId;
         this.id = id;
         this.amount = amount;
@@ -59,56 +62,6 @@ public class Transaction {
 
     public Transaction getPreviousTransaction() {
         return this.previousTransaction;
-    }
-
-    public boolean equals(Object o) {
-        if (o == this) {
-            return true;
-        }
-        if (!(o instanceof Transaction)) {
-            return false;
-        }
-        final Transaction other = (Transaction) o;
-        final Object this$accountId = this.getAccountId();
-        final Object other$accountId = other.getAccountId();
-        if (this$accountId == null ? other$accountId != null : !this$accountId.equals(other$accountId)) {
-            return false;
-        }
-        final Object this$id = this.getId();
-        final Object other$id = other.getId();
-        if (this$id == null ? other$id != null : !this$id.equals(other$id)) {
-            return false;
-        }
-        final Object this$amount = this.getAmount();
-        final Object other$amount = other.getAmount();
-        if (this$amount == null ? other$amount != null : !this$amount.equals(other$amount)) {
-            return false;
-        }
-        final Object this$previousTransaction = this.getPreviousTransaction();
-        final Object other$previousTransaction = other.getPreviousTransaction();
-        if (this$previousTransaction == null ? other$previousTransaction != null : !this$previousTransaction.equals(other$previousTransaction)) {
-            return false;
-        }
-        return true;
-    }
-
-    public int hashCode() {
-        final int PRIME = 59;
-        int result = 1;
-        final Object $accountId = this.getAccountId();
-        result = result * PRIME + ($accountId == null ? 43 : $accountId.hashCode());
-        final Object $id = this.getId();
-        result = result * PRIME + ($id == null ? 43 : $id.hashCode());
-        final Object $amount = this.getAmount();
-        result = result * PRIME + ($amount == null ? 43 : $amount.hashCode());
-        final Object $previousTransaction = this.getPreviousTransaction();
-        result = result * PRIME + ($previousTransaction == null ? 43 : $previousTransaction.hashCode());
-        return result;
-    }
-
-    public String toString() {
-        return "Transaction(accountId=" + this.getAccountId() + ", id=" + this.getId() + ", amount=" + this.getAmount() + ", previousTransaction=" + this
-            .getPreviousTransaction() + ")";
     }
 
     static class TransactionBuilder {
