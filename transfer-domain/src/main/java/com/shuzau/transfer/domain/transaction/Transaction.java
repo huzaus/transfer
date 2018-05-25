157f1ffaeb1f7384ac6a34da150bd6473b580457
huzaus <siarhei.huzau@grandparade.co.uk>
2018-05-25 08:44:49 +0200

    Remove Transfer object from transaction package

--- transfer-domain/src/main/java/com/shuzau/transfer/domain/transaction/Transaction.java
+++ transfer-domain/src/main/java/com/shuzau/transfer/domain/transaction/Transaction.java
@@ -2,77 +2,92 @@ package com.shuzau.transfer.domain.transaction;
 
 import com.shuzau.transfer.domain.core.AccountId;
 import com.shuzau.transfer.domain.core.Money;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import lombok.NonNull;
+import lombok.ToString;
 
-public interface Transaction {
+@EqualsAndHashCode
+@ToString
+@Getter
+public class Transaction {
 
+    @NonNull
+    private final AccountId accountId;
+    @NonNull
+    private final TransactionId id;
+    @NonNull
+    private final Money amount;
 
-    static Builder createNewAccountTransaction(@NonNull AccountId accountId, @NonNull Money initialBalance) {
-        return BasicTransactionBuilder.builder()
-                                      .accountId(accountId)
-                                      .amount(initialBalance);
+    private final Transaction previousTransaction;
+
+    private Transaction(AccountId accountId, TransactionId id, Money amount, Transaction previousTransaction) {
+        this.accountId = accountId;
+        this.id = id;
+        this.amount = amount;
+        this.previousTransaction = previousTransaction;
     }
 
-    boolean isInitial();
-
-    AccountId getAccountId();
-
-    TransactionId getId();
-
-    Money getAmount();
-
-    Transaction getPreviousTransaction();
-
-    default Builder nextWithdrawTransaction(@NonNull Money amount) {
-        return BasicTransactionBuilder.builder()
-                                      .previousTransaction(this)
-                                      .accountId(getAccountId())
-                                      .amount(amount.negate());
+    public static Builder createNewAccountTransaction(@NonNull AccountId accountId, @NonNull Money initialBalance) {
+        return TransactionBuilder.builder()
+                                 .accountId(accountId)
+                                 .amount(initialBalance);
     }
 
-    default Builder nextDepositTransaction(@NonNull Money amount) {
-        return BasicTransactionBuilder.builder()
-                                      .previousTransaction(this)
-                                      .accountId(getAccountId())
-                                      .amount(amount);
+    public boolean isInitial() {
+        return previousTransaction == null;
     }
 
-    interface Builder {
+    public Builder nextWithdrawTransaction(@NonNull Money amount) {
+        return TransactionBuilder.builder()
+                                 .previousTransaction(this)
+                                 .accountId(getAccountId())
+                                 .amount(amount.negate());
+    }
+
+    public Builder nextDepositTransaction(@NonNull Money amount) {
+        return TransactionBuilder.builder()
+                                 .previousTransaction(this)
+                                 .accountId(getAccountId())
+                                 .amount(amount);
+    }
+
+    public interface Builder {
 
         Transaction withId(@NonNull TransactionId id);
     }
 
-    class BasicTransactionBuilder implements Builder {
+    private static class TransactionBuilder implements Builder {
 
         private AccountId accountId;
         private Money amount;
         private Transaction previousTransaction;
 
-        private BasicTransactionBuilder() {
+        private TransactionBuilder() {
         }
 
-        static BasicTransactionBuilder builder() {
-            return new BasicTransactionBuilder();
+        private static TransactionBuilder builder() {
+            return new TransactionBuilder();
         }
 
-        private BasicTransactionBuilder accountId(AccountId accountId) {
+        private TransactionBuilder accountId(AccountId accountId) {
             this.accountId = accountId;
             return this;
         }
 
-        private BasicTransactionBuilder amount(Money amount) {
+        private TransactionBuilder amount(Money amount) {
             this.amount = amount;
             return this;
         }
 
-        private BasicTransactionBuilder previousTransaction(Transaction previousTransaction) {
+        private TransactionBuilder previousTransaction(Transaction previousTransaction) {
             this.previousTransaction = previousTransaction;
             return this;
         }
 
         @Override
         public Transaction withId(@NonNull TransactionId id) {
-            return new BasicTransaction(accountId, id, amount, previousTransaction);
+            return new Transaction(accountId, id, amount, previousTransaction);
         }
     }
 }
--- transfer-domain/src/main/java/com/shuzau/transfer/domain/transaction/TransferType.java
+++
@@ -1,7 +0,0 @@
-package com.shuzau.transfer.domain.transaction;
-
-public enum TransferType {
-
-    IN,
-    OUT
-}
